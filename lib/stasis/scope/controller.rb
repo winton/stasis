a7bdc876e94cc9af4da741a704770354fc2d250e
Bernerd Schaefer <bj.schaefer@gmail.com>
2012-04-03 12:24:06 +0200

    Remove Dir.chdir calls

--- lib/stasis/scope/controller.rb
+++ lib/stasis/scope/controller.rb
@@ -23,9 +23,6 @@ class Stasis
       # Temporarily set path variables.
       @_stasis.path = path
 
-      # Change current working directory.
-      Dir.chdir(File.dirname(path))
-      
       # Evaluate `controller.rb`.
       instance_eval(File.read(path), path)
 
